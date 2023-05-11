4db75b530fb224d74d433e7c48b7a74d2118c15c
_Y_Chan <loccoc1807@gmail.com>
2023-05-11 16:42:30 +0700

    Add Sign In controller, view

--- PBL3_MVC/Controllers/AuthController.cs
+++ PBL3_MVC/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PBL3_MVC.Data;
+using PBL3_MVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +11,39 @@ namespace PBL3_MVC.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly BookingBusEntities _db = new BookingBusEntities();
+
+        //public AuthController(BookingBusEntities db) { 
+        //    _db = db;
+        //}
+
         // GET: Auth
         public ActionResult Index()
         {
             return View();
         }
 
-
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignIn(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var Account = _db.Accounts.Where(account => account.UserName == model.username && account.Password == model.password).FirstOrDefault();
+                if (Account == null) {
+                    ModelState.AddModelError("", "Nhập sai tài khoản hoặc mật khẩu");
+                    return View(model);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
     }
 }
