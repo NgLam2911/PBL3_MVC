918349b5b1e68f37bdd6c776bfa7d251ee443074
NDLoc-vn <loccoc1807@gmail.com>
2023-05-25 20:56:47 +0700

    Add Booking Bus by Customer

---
+++ PBL3_MVC/Models/BillModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PBL3_MVC.Models
+{
+    public class BillModel
+    {
+        public int SeatID { get; set; }
+        public string CustomerName { get; set; }
+        public string BusStationName { get; set; }
+        public string BusName { get; set; }
+        public int SeatNumber { get; set; }
+        public string Departure { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime DestinationTime { get; set; }
+        public double Price { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
